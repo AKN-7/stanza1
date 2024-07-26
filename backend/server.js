1d016a850c213ea4effc9880e4c3bbbf46cf64b8
AKN-7 <a.neami05@gmail.com>
2024-07-25 20:18:42 -0400

    tried adding vercel json

--- backend/server.js
+++ backend/server.js
@@ -31,25 +31,25 @@ const poemSchema = new mongoose.Schema({
 
 const Poem = mongoose.model('Poem', poemSchema);
 
-app.post('/poems', async (req, res) => {
+app.post('/api/poems', async (req, res) => {
   const poem = new Poem(req.body);
   await poem.save();
   res.status(201).send(poem);
 });
 
-app.get('/poems', async (req, res) => {
+app.get('/api/poems', async (req, res) => {
   const poems = await Poem.find().sort({ likes: -1, date: -1 });
   res.send(poems);
 });
 
-app.post('/poems/:id/like', async (req, res) => {
+app.post('/api/poems/:id/like', async (req, res) => {
   const poem = await Poem.findById(req.params.id);
   poem.likes += 1;
   await poem.save();
   res.send(poem);
 });
 
-app.delete('/poems', async (req, res) => {
+app.delete('/api/poems', async (req, res) => {
   await Poem.deleteMany({});
   res.status(200).send({ message: 'All poems cleared' });
 });
